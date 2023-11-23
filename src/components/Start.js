b1d997fc326e1c53b73387214a588b73af9fec1c
Waldemar Naposo <waldemarnaposo313@gmail.com>
2023-11-23 10:13:07 +0700

    first commit

---
+++ src/components/Start.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Start = () => {
+    return(
+        <div className="card">
+            <div className="card-content">
+                <h1>Mulai Quiz</h1>
+                <p>Semoga Lancar...</p>
+                <button className="button is-success is-medium">Mulai</button>
+            </div>
+        </div>
+    )
+}
+export default Start;
